be94bf763226c3561722b24c5c30c300a1bb693b
Josh Goodman <jogoodma@iu.edu>
2019-01-11 14:02:40 -0500

    General code cleanup.

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -1,29 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Block({ data, baseRGB, heatLevels, onTermClick }) {
-  function heatColor(heat, rgb, levels) {
-    // return 'white' if heat==0 (which should never happen)
-    if (heat == 0) return '#fff'
-    let c = [] // [r,g,b]
-    for (var i = 0; i < 3; i++) {
-      // logarithmic heatmap (with cutoff)
-      if (heat < levels) {
-        // instead of just (256-rgb[i])/(Math.pow(2,heat)), which divides space from
-        // 'white' (255) down to target color level in halves, this starts at 3/4
-        const heatCoef = (3 * (256 - rgb[i])) / Math.pow(2, heat + 1)
-        c[i] = Math.round(rgb[i] + heatCoef)
-      } else {
-        c[i] = rgb[i]
-      }
-      // linear heatmap
-      // var heatInc = (topHue-rgb[i])/heatLevels;
-      // var depression = heatInc*Math.min(heat,heatLevels);
-      // c[i] = Math.round(topHue - depression);
+function heatColor(heat, rgb, levels) {
+  // return 'white' if heat==0 (which should never happen)
+  if (heat == 0) return '#fff'
+  const c = [0, 0, 0] // [r,g,b]
+  for (let i = 0; i < 3; i++) {
+    // logarithmic heatmap (with cutoff)
+    if (heat < levels) {
+      // instead of just (256-rgb[i])/(Math.pow(2,heat)), which divides space from
+      // 'white' (255) down to target color level in halves, this starts at 3/4
+      const heatCoef = (3 * (256 - rgb[i])) / Math.pow(2, heat + 1)
+      c[i] = Math.round(rgb[i] + heatCoef)
+    } else {
+      c[i] = rgb[i]
     }
-    return 'rgb(' + c[0] + ',' + c[1] + ',' + c[2] + ')'
   }
+  return `rgb(${c.join(',')})`
+}
 
+function Block({ data, baseRGB, heatLevels, onTermClick }) {
   function handleOnClick(evt) {
     if (onTermClick) {
       onTermClick(data, evt)
@@ -33,7 +29,6 @@ function Block({ data, baseRGB, heatLevels, onTermClick }) {
   const blockTitle = data.name
   const tileStrength = data.descendant_terms.length
   const s = tileStrength == 1 ? '' : 's'
-  const tileTitle = blockTitle + ':\n' + tileStrength + ' term' + s
   const blockTitleClass =
     tileStrength > 0 ? 'ribbonBlockTitleTerm bold' : 'ribbonBlockTitleTerm'
   const color = tileStrength ? heatColor(tileStrength, baseRGB, heatLevels) : ''
@@ -43,7 +38,7 @@ function Block({ data, baseRGB, heatLevels, onTermClick }) {
       <div className={blockTitleClass}>{blockTitle}</div>
       <div
         className="ribbonTile"
-        title={tileTitle}
+        title={`${blockTitle}:\n${tileStrength} term ${s}`}
         style={{ backgroundColor: color }}
       />
     </div>
@@ -66,12 +61,7 @@ Block.propTypes = {
     (propValue, key, componentName, location, propFullName) => {
       if (propValue.length != 3) {
         return new Error(
-          'Invalid prop `' +
-            propFullName +
-            '` supplied to' +
-            ' `' +
-            componentName +
-            '`. An array of 3 integers is required.'
+          `Invalid prop ${propFullName} supplied to ${componentName}.  An array of 3 integers is required.`
         )
       }
     }
